ee4581a86138c13ea1927cb5051762273f3461ff
astrelatte <bandettinialex@gmail.com>
2023-12-19 21:47:17 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const caixa  = document.querySelector('.caixa');
+
+const ligarEDesligarADiv = function() {
+
+    caixa.style.display === "flex" ? caixa.style.display = "none": caixa.style.display = "flex"
+};
